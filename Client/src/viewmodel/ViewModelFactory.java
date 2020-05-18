f40721c71f71d9ffe128cbd923baa316a2c023ab
pavliuc75 <56117353+pavliuc75@users.noreply.github.com>
2020-05-18 19:30:25 +0200

    addedRegister

--- Client/src/viewmodel/ViewModelFactory.java
+++ Client/src/viewmodel/ViewModelFactory.java
@@ -2,18 +2,26 @@ package viewmodel;
 
 import model.ClientModel;
 import viewmodel.login.LoginViewModel;
+import viewmodel.login.RegistrationViewModel;
 
 public class ViewModelFactory
 {
   private LoginViewModel loginViewModel;
+  private RegistrationViewModel registrationViewModel;
 
   public ViewModelFactory(ClientModel model)
   {
     loginViewModel = new LoginViewModel(model);
+    registrationViewModel = new RegistrationViewModel(model);
   }
 
   public LoginViewModel getLoginViewModel()
   {
     return loginViewModel;
+  }
+
+  public RegistrationViewModel getRegistrationViewModel()
+  {
+    return registrationViewModel;
   }
 }
---
+++ Client/src/viewmodel/login/RegistrationViewModel.java
@@ -0,0 +1,43 @@
+package viewmodel.login;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import model.ClientModel;
+
+public class RegistrationViewModel
+{
+  private ClientModel model;
+  private StringProperty username;
+  private StringProperty password;
+  private StringProperty repeatPassword;
+
+  public RegistrationViewModel(ClientModel model)
+  {
+    this.model = model;
+    username = new SimpleStringProperty();
+    password = new SimpleStringProperty();
+    repeatPassword = new SimpleStringProperty();
+  }
+
+  public void clear()
+  {
+    username.set(null);
+    password.set(null);
+    repeatPassword.set(null);
+  }
+
+  public StringProperty getUsernameProperty()
+  {
+    return username;
+  }
+
+  public StringProperty getPasswordProperty()
+  {
+    return password;
+  }
+
+  public StringProperty getRepeatPasswordProperty()
+  {
+    return repeatPassword;
+  }
+}
