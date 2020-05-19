aec6242ccf17671cea8c5dd57e4765bb5c3efde2
pavliuc75 <56117353+pavliuc75@users.noreply.github.com>
2020-05-19 19:41:43 +0200

    1941

--- Client/src/view/discussionList/MainViewController.java
+++ Client/src/view/discussionList/MainViewController.java
@@ -1,12 +1,17 @@
 package view.discussionList;
 
+import com.jfoenix.controls.JFXListView;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import view.ViewHandler;
 import viewmodel.discussionList.MainViewModel;
 
 public class MainViewController
 {
+  @FXML private JFXListView<Label> listView;
+
   private ViewHandler viewHandler;
   private MainViewModel viewModel;
   private Region root;
@@ -17,6 +22,12 @@ public class MainViewController
     this.viewHandler = viewHandler;
     this.viewModel = viewModel;
     this.root = root;
+
+    for (int i = 0; i < 20; i++)
+    {
+      Label label = new Label("Item: " + i);
+      listView.getItems().add(label);
+    }
   }
 
   public void reset()
