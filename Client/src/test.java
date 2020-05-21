cc6ac1bfbe47082a1718deb32ed4a3daf35a3944
Eugene4JD <36538202+Eugene4JD@users.noreply.github.com>
2020-05-21 14:03:16 +0200

    dd

--- Client/src/test.java
+++ Client/src/test.java
@@ -1,7 +1,87 @@
+import mediator.RemoteModel;
+import mediator.RemoteModelProxy;
+import model.ClientModel;
+import model.ClientModelManager;
+import java.util.Scanner;
+
 public class test
 {
+  //  1 void connect() throws IOException;
+  //  2 void disconnect() throws IOException;
+  //  3 void log(String login, String password, boolean isNewUser);
+  //  4 void logToExistingDiscussion(String discussionId,String login);
+  //  5 void createNewDiscussion(String discussionId,String loginOfEditor);
+  //  6 void sendMessageInDiscussion(String discussionId, String sender, String message);
+  //  7 void removeDiscussion(String discussionId);
   public static void main(String[] args)
   {
-    System.out.println("test");
+    try
+    {
+      ClientModel model=new ClientModelManager();
+      Scanner in = new Scanner(System.in);
+      System.out.println("Which function to test enter number");
+      while (true)
+      {
+        int functionID = in.nextInt();
+        switch (functionID)
+        {
+          case 1:
+            model.connect();
+            System.out.println("model connected");
+            break;
+          case 2:
+            model.disconnect();
+            System.out.println("model disconnected");
+            break;
+          case 3:
+            in.nextLine();
+            System.out.println("write String login");
+            String login=in.nextLine();
+            System.out.println("Write String password");
+            String password=in.nextLine();
+            System.out.println("Write boolean isNewUser");
+            boolean isNewUser=in.nextBoolean();
+            model.log(login,password,isNewUser);
+            System.out.println("log added");
+            break;
+          case 4:
+            System.out.println("write String discussionId");
+            String discussionId=in.nextLine();
+            System.out.println("Write String login");
+            String login1=in.nextLine();
+            System.out.println("logToExistingDiscussion");
+            break;
+          case 5:
+            System.out.println("write String discussionId");
+            String discussionId1=in.nextLine();
+            System.out.println("Write String loginOfEditor");
+            String loginOfEditor=in.nextLine();
+            System.out.println("createNewDiscussion completed");
+            break;
+          case 6:
+            System.out.println("write String discussionId");
+            String discussionId2=in.nextLine();
+            System.out.println("Write String sender");
+            String sender=in.nextLine();
+            System.out.println("Write String message");
+            String message=in.nextLine();
+            System.out.println("sendMessageInDiscussion compelted");
+            break;
+          case 7:
+            System.out.println("write String discussionId");
+            String discussionId3=in.nextLine();
+            model.removeDiscussion(discussionId3);
+            System.out.println("removed discussion");
+            break;
+          default:
+            System.out.println("There are no function for this number");
+        }
+      }
+    }
+    catch (Exception e)
+    {
+
+    }
   }
 }
+
--- Server/src/SERVER.java
+++ Server/src/SERVER.java
@@ -1,7 +1,16 @@
+import mediator.ServerClientHandler;
+import mediator.ServerConnector;
+import model.ServerModel;
+import model.ServerModelManager;
+
 public class SERVER
 {
   public static void main(String[] args)
   {
-    System.out.println("test");
+    ServerModel model = new ServerModelManager();
+    ServerConnector connector = new ServerConnector(model);
+    Thread thread1 = new Thread(connector);
+    thread1.start();
+
   }
 }
