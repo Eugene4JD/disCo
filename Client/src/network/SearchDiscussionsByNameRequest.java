d19aec08f55d923cb7f83cc04dde6ddd64c5d4c1
Eugene4JD <36538202+Eugene4JD@users.noreply.github.com>
2020-05-26 22:36:31 +0200

    ds

--- Client/src/network/SearchDiscussionsByNameRequest.java
+++ Client/src/network/SearchDiscussionsByNameRequest.java
@@ -3,6 +3,7 @@ package network;
 public class SearchDiscussionsByNameRequest extends Request
 {
   String name;
+
   public SearchDiscussionsByNameRequest(String name)
   {
     super(RequestType.SearchDiscussionsByName);
--- Server/src/network/BroadcastDiscussionsToUserRequest.java
+++ Server/src/network/BroadcastDiscussionsToUserRequest.java
@@ -5,11 +5,13 @@ import model.DiscussionList;
 public class BroadcastDiscussionsToUserRequest extends Request
 {
   private DiscussionList discussions;
+
   public BroadcastDiscussionsToUserRequest(DiscussionList discussionList)
   {
     super(RequestType.BroadcastDiscussions);
     this.discussions = discussionList;
   }
+
   public DiscussionList getDiscussions()
   {
     return this.discussions;
