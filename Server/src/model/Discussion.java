c95db118223e91febb970960e5640b09f2751b89
Eugene4JD <36538202+Eugene4JD@users.noreply.github.com>
2020-06-01 21:14:06 +0200

    JAVA DOC

--- Server/src/model/Discussion.java
+++ Server/src/model/Discussion.java
@@ -1,5 +1,11 @@
 package model;
 
+/**
+ * A class representing discussion
+ *
+ * @author Group2
+ * @version 1.1
+ */
 public class Discussion
 {
   private MessageList messageList;
@@ -8,6 +14,13 @@ public class Discussion
   private String discussionName;
   private String LoginOfEditorOfDiscussion;
 
+  /**
+   * Three arguments constructor setting the discussion id, discussion name and editor of discussion
+   *
+   * @param discussionId       the discussion id
+   * @param discussionName     the discussion name
+   * @param editorOfDiscussion the editor of discussion
+   */
   public Discussion(int discussionId, String discussionName,
       String editorOfDiscussion)
   {
@@ -18,56 +31,116 @@ public class Discussion
     this.discussionName = discussionName;
   }
 
+  /**
+   * Adding user to this discussion userBase
+   *
+   * @param user the user
+   */
   public void addUser(User user)
   {
     userBase.addUser(user);
   }
 
+  /**
+   * Adding user with attributes which will be used when creating this new user in userBase
+   *
+   * @param id       the id of user
+   * @param type     the type of user
+   * @param login    the login of user
+   * @param password the password of user
+   */
   public void addUser(int id, String type, String login, String password)
   {
     userBase.addUser(id, type, login, password);
   }
 
+  /**
+   * removing user from the discussion userBase by userID
+   *
+   * @param userId the user Id
+   */
   public void removeUserById(int userId)
   {
     userBase.removeUserById(userId);
   }
 
+  /**
+   * the user by given userId from this discussion UserBase
+   *
+   * @param userId user Id
+   * @return the user by given userId from this discussion UserBase
+   */
   public User getUserIdByLogin(int userId)
   {
     return userBase.getUserById(userId);
   }
 
+  /**
+   * adding the message to the Message list of this discussion
+   *
+   * @param message   string containing the message itself
+   * @param messageId the message id
+   */
   public void addMessage(String message, int messageId)
   {
     messageList.addMessage(message, messageId);
   }
 
+  /**
+   * returns message list of this discussion
+   *
+   * @return message list of this discussion
+   */
   public MessageList getMessageList()
   {
     return messageList;
   }
 
+  /**
+   * returns user Base of this discussion
+   *
+   * @return user Base of this discussion
+   */
   public UserBase getUserBase()
   {
     return userBase;
   }
 
+  /**
+   * returns Discussion name
+   *
+   * @return Discussion name
+   */
   public String getDiscussionName()
   {
     return discussionName;
   }
 
+  /**
+   * returns discussion Id
+   *
+   * @return discussion id
+   */
   public int getDiscussionId()
   {
     return discussionId;
   }
 
+  /**
+   * sets the name of this discussion to given
+   *
+   * @param discussionName discussion
+   */
   public void setDiscussionName(String discussionName)
   {
     this.discussionName = discussionName;
   }
 
+  /**
+   * returns login of editor of this discussion
+   *
+   * @return
+   */
   public String getLoginOfEditorOfDiscussion()
   {
     return this.LoginOfEditorOfDiscussion;
--- Server/src/model/MessageList.java
+++ Server/src/model/MessageList.java
@@ -2,20 +2,39 @@ package model;
 
 import java.util.ArrayList;
 
+/**
+ * Class representing message list
+ *
+ * @author Group 2
+ * @version 1.1
+ */
 public class MessageList
 {
   private ArrayList<Message> messages;
 
+  /**
+   * Zero argument constructor
+   * initializing the array list of message
+   */
   public MessageList()
   {
     messages = new ArrayList<>();
   }
 
+  /**
+   * adding new message to this message list
+   *
+   * @param text      text of the message
+   * @param messageId message id
+   */
   public void addMessage(String text, int messageId)
   {
     messages.add(new Message(text, messageId));
   }
 
+  /**
+   * clearing the message list by deleting every message from it
+   */
   public void clearList()
   {
     int index = messages.size();
@@ -24,18 +43,26 @@ public class MessageList
       messages.remove(0);
     }
   }
+
+  /**
+   * returning the message by given message id
+   *
+   * @param index given message id
+   * @return the message by given message id
+   */
   public Message getMessage(int index)
   {
     return messages.get(index);
   }
 
+  /**
+   * returning the message list size
+   *
+   * @return size of message list
+   */
   public int size()
   {
     return messages.size();
   }
 
-  private int getNewId()
-  {
-    return this.messages.size() + 1;
-  }
 }
